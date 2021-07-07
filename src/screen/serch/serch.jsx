57d5b2bce8bf5f70c54e105f5eb9f0096c29bb24
haejii <yanghagi@naver.com>
2021-07-07 14:56:28 +0900

    Improve Re-rendering Issue

--- src/screen/serch/serch.jsx
+++ src/screen/serch/serch.jsx
@@ -1,40 +1,42 @@
-import React, { useReducer, useRef } from 'react';
+import React, {memo, useRef } from 'react';
 import styles from './serch.module.css';
 
-const Serch = ({onSearch}) => {
+const Serch = memo(
+    ({onSearch} ) => {
 
-    const inputRef = useRef();
-    const handleSearch = ()=>{
-        const value = inputRef.current.value;
-        onSearch(value);
-    }
-    const onClick =() =>{
-        handleSearch();
-    }
-
-    const onKeyPress = (event) =>{
-        if(event.key === 'Enter'){
+        const inputRef = useRef();
+        const handleSearch = ()=>{
+            const value = inputRef.current.value;
+            onSearch(value);
+        }
+        const onClick =() =>{
             handleSearch();
         }
+    
+        const onKeyPress = (event) =>{
+            if(event.key === 'Enter'){
+                handleSearch();
+            }
+        }
+        return(
+            <header className={styles.header}>
+                <div className={styles.logos}>
+                <img className={styles.logo} src="/img/logo.png" alt="logo"/>
+                <h1 className={styles.title}>Youtube</h1>
+                </div>
+                <input 
+                ref = {inputRef}
+                className={styles.serch} 
+                type="serch" 
+                placeholder="Search..."
+                onKeyPress={onKeyPress}
+                />
+                <button className={styles.submitBtn} type="submit" onClick={onClick}>
+                    <img className={styles.serchIcon} src="/img/search.png" alt="serach"/>
+                </button>
+            </header>
+        );
     }
-    return(
-        <header className={styles.header}>
-            <div className={styles.logos}>
-            <img className={styles.logo} src="/img/logo.png" alt="logo"/>
-            <h1 className={styles.title}>Youtube</h1>
-            </div>
-            <input 
-            ref = {inputRef}
-            className={styles.serch} 
-            type="serch" 
-            placeholder="Search..."
-            onKeyPress={onKeyPress}
-            />
-            <button className={styles.submitBtn} type="submit" onClick={onClick}>
-                <img className={styles.serchIcon} src="/img/search.png" alt="serach"/>
-            </button>
-        </header>
-    )
-}
+);
 
 export default Serch;
--- src/screen/video_item/video_item.jsx
+++ src/screen/video_item/video_item.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({video, video :{snippet}, onVideoClick, display}) => {
+const VideoItem = memo(({video, video :{snippet}, onVideoClick, display}) => {
     
     const displayType = display === 'list' ? styles.list : styles.grid;
     return(
@@ -21,5 +21,7 @@ const VideoItem = ({video, video :{snippet}, onVideoClick, display}) => {
    
     
 </li>
-)};
+);
+}
+)
 export default VideoItem;
